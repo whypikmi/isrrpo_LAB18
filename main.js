37a66c4a4d940ebba5a988a882216d1fe2b64121
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-03-13 13:51:03 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -173,8 +173,72 @@ console.log(x1 + y1);
 console.log(x1 - y1);
 console.log(x1 * y1);
 console.log(x1 / y1);
-*/
 let a2 = 15;
 let b2 = 3;
 console.log(a2 + b2);
 console.log(a2 * b2);
+if (isStudent && age < 25) {
+    console.log("Доступна студенческая скидка");
+}
+let message = age >= 18? "Совершеннолетний" : "Несовершеннолетний";
+console.log(message);
+*/
+let yourAge = 18;
+if (yourAge >= 18) {
+    console.log("Доступ разрешён");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let temperature = -6;
+if (temperature < 0) {
+    console.log("Холодно");
+}
+else if (temperature >= 0 && temperature < 20) {
+    console.log("Прохладно");
+}
+else {
+    console.log("Тепло");
+}
+
+let isLoggedIn = true;
+let isAdmin = true;
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+}
+else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченный доступ");
+}
+else {
+    console.log("Доступ запрещён");
+}
+
+let a3 = 10;
+let b3 = "10";
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let day = 3;
+switch (day) {
+    case 1: console.log("Понедельник"); break;
+    case 2: console.log("Вторник"); break;
+    case 3: console.log("Среда"); break;
+    default: console.log("Неизвестный день");
+}
+
+let monthNumber = 5;
+switch (monthNumber) {
+    case 1: console.log("Январь"); break;
+    case 2: console.log("Февраль"); break;
+    case 3: console.log("Март"); break;
+    case 4: console.log("Апрель"); break;
+    case 5: console.log("Май");break;
+    case 6:console.log("Июнь");break;
+    case 7:console.log("Июль");break;
+    case 8:console.log("Август");break;
+    case 9:console.log("Сентябрь");break;
+    case 10:console.log("Октябрь");break;
+    case 11:console.log("Ноябрь");break;
+    case 12:console.log("Декабрь");break;
+    default:console.log("Введите число от 1 до 12");
+}
